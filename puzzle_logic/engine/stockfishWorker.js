118e254b8e422822b1f35e01f2cdc7a09d786c5f
HitenKhialani <co2023.hiten.khialani@ves.ac.in>
2025-06-21 12:10:09 +0530

    loigc deleted

--- puzzle_logic/engine/stockfishWorker.js
+++
@@ -1,8 +0,0 @@
-export function createStockfish() {
-  // Use the public path for the Stockfish worker
-  const worker = new Worker('/engine/stockfish.worker.js');
-  worker.postMessage('uci');
-  worker.postMessage('setoption name MultiPV value 2');
-  worker.postMessage('isready');
-  return worker;
-}
--- puzzle_logic/utils/validatePuzzle.js
+++
@@ -1,4 +0,0 @@
-export function validatePuzzle(best, second) {
-  const evalGap = best.eval - second.eval;
-  return evalGap > 200; // Accept puzzles with clear best move
-}
